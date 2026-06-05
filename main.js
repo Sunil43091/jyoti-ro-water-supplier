92c5c7a9c6d05c183a1acd4872b8de2a2f9301ea
Sunil43091 <102575989+Sunil43091@users.noreply.github.com>
2026-06-05 17:35:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -40,3 +40,53 @@ navMenu.classList.remove(
 });
 
 });
+
+const bar =
+document.getElementById("progressBar");
+
+const text =
+document.getElementById("progressText");
+
+const wave =
+document.querySelector(".wave");
+
+const wave2 =
+document.querySelector(".wave2");
+
+let progress = 0;
+
+const loading = setInterval(()=>{
+
+progress++;
+
+bar.style.width =
+progress + "%";
+
+text.innerHTML =
+progress + "%";
+
+const level =
+-170 + (progress * 1.4);
+
+wave.style.bottom =
+level + "px";
+
+wave2.style.bottom =
+(level - 10) + "px";
+
+if(progress >= 100){
+
+clearInterval(loading);
+
+setTimeout(()=>{
+
+document
+.getElementById("loader")
+.classList
+.add("loader-hide");
+
+},400);
+
+}
+
+},20);
